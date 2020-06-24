2354adf43e6feea3dcfad39cd412d9babe2b448f
harshwalia36 <harsh.p.walia@gmail.com>
2020-06-24 07:46:07 +0530

    remove build from gitignore

---
+++ frontend/build/precache-manifest.65fa37a4774b136173f33e97e9c7a6ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd710f489f2ff7b7124dadd93b63eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e1e039ccb26bf048f97",
+    "url": "/static/css/main.d8fff77c.chunk.css"
+  },
+  {
+    "revision": "343d29d64999c23a92e6",
+    "url": "/static/js/2.28fbcf77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.28fbcf77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1e039ccb26bf048f97",
+    "url": "/static/js/main.66fc8978.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
